96900964dbfb272dae45acd8744b53ed87e7b070
Alyssa <125580811+ajpupping@users.noreply.github.com>
2024-03-16 22:15:34 -0700

    updated resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,11 +1,13 @@
-const { User, Book } = require('../models');
+// resolvers.js
 const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
         me: async (_, args, context) => {
             if (!context.user) {
-                throw new AuthenticationError('Not logged in');
+                throw new AuthenticationError('Please log in to continue.');
             }
             return User.findById(context.user._id);
         },
@@ -16,24 +18,45 @@ const resolvers = {
             const token = signToken(user);
             return { token, user };
         },
-        loginUser: async (_, { email, password }) => {
+        login: async (_, { email, password }) => {
             const user = await User.findOne({ email });
             if (!user) {
-                throw new AuthenticationError('Incorrect credentials');
+                throw new AuthenticationError('Incorrect username or password');
             }
+
             const correctPw = await user.isCorrectPassword(password);
             if (!correctPw) {
-                throw new AuthenticationError('Incorrect credentials');
+                throw new AuthenticationError('Incorrect username or password');
             }
+
             const token = signToken(user);
             return { token, user };
         },
-
-    },
-
-        me: async (_, args, context) => {
-            if (!context.authData) {
-                throw new Error('Not authenticated');
+        saveBook: async (_, { bookInput }, context) => {
+            if (!context.user) {
+                throw new AuthenticationError('Please log in and try again.');
             }
+
+            const updatedUser = await User.findByIdAndUpdate(
+                { _id: context.user._id },
+                { $addToSet: { savedBooks: bookInput } },
+                { new: true, runValidators: true }
+            );
+            return updatedUser;
         },
+        removeBook: async (_, { bookId }, context) => {
+            if (!context.user) {
+                throw new AuthenticationError('Please log in and try again.');
+            }
+
+            const updatedUser = await User.findByIdAndUpdate(
+                { _id: context.user._id },
+                { $pull: { savedBooks: { bookId } } },
+                { new: true }
+            );
+            return updatedUser;
+        },
+    },
 };
+
+module.exports = resolvers;
