91251decb972acaa0732f59a663a92ad1ca3d255
Alyssa <125580811+ajpupping@users.noreply.github.com>
2024-03-16 22:20:15 -0700

    updated index to export typeDefs and resolvers

---
+++ server/schemas/index.js
@@ -0,0 +1,8 @@
+// import typeDefs 
+const typeDefs = require('./typeDefs');
+
+// import resolvers
+const resolvers = require('./resolvers');
+
+// export typeDefs and resolvers
+module.exports = { typeDefs, resolvers };
